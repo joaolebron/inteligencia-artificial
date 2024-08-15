7a7296fce6a3281bc2d10d128645702eba119544
joaolebron <131272140+joaolebron@users.noreply.github.com>
2024-08-15 09:55:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,8 +6,8 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
-        alternativas: [
+        enunciado: "+o que a internet influencia nas drogas?",
+        alternativas: [a internet ajuda na divulgação e onde as pessoas tem mais Acesso
             {
                 texto: "Isso é assustador!",
                 afirmacao: "afirmação"
